e71b0d79c95b6873382ffff92c0d44f1d53bb26e
David <davidliv6@gmail.com>
2024-03-09 13:42:46 +0100

    Version Finale

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+const ButtonDiv = document.getElementById("ButtonDiv")
 const button = document.getElementById("button")
     button.addEventListener("click", function() {
-        button.style.display = "none";
+        ButtonDiv.style.display = "none";
         const IntroDiv = document.getElementById("Intro");
-        videoID = videoBG(IntroDiv);
+        const videoID = videoBG(IntroDiv, './Sound.mp3','./BG.mp4');
         two = false, four = false, six = false, eight = false, ten = false, twelve = false, fourteen = false, sixteen = false, eighteen = false, twentyfour = false;
         videoID.addEventListener('timeupdate', function() {
             if (videoID.currentTime >= 2 && two == false) {
@@ -57,7 +58,8 @@ const button = document.getElementById("button")
                 twentyfour = true;
                 
                 animation = document.getElementById("CreditID");
-                animation.style.animation = "translate 120s linear forwards";
+                animation.style.animation = "translate 157s linear forwards";
+                CreditsIMG()
                 Credits(allIMG);
             }
 
@@ -65,21 +67,39 @@ const button = document.getElementById("button")
                 LogoDiv = document.getElementById("LogoDiv");
                 LogoDiv.style.transform = "translate(-50%,-50%) scale(0)";
                 LogoDiv.style.opacity = 0;
-                videoID.removeEventListener('timeupdate', arguments.callee);
             }
+        });
+        videoID.addEventListener("ended",function(){
+            const videoID2 = videoBG(IntroDiv, './SoundTwo.mp3', './BG2.mp4');
         });
     });
 
-const videoBG = function(IntroDiv){
-    const audio = new Audio('./Sound.mp3');
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+const videoBG = function(IntroDiv, Sound, Video){
+    const audio = new Audio(Sound);
     const video = document.createElement('video');
-    video.src = './BG.mp4';
+    video.src = Video;
     video.muted = true;                
     video.classList.add("videoBG");
     IntroDiv.appendChild(video);
     video.play();
     audio.play();
+    if (Sound == './SoundTwo.mp3'){
+        audioBG(audio, video);
+        return video
+    };
     return video;
+}
+
+const audioBG = async function(audio, video){
+    for (let i = 0; i <= 50; i++) {
+        audio.volume = 0.005 * i;
+        video.style.filter = `brightness(${0.01*i}) blur(${0.2*i}px)`;
+        await sleep(200);
+    }
 }
 
 /* Element de la vidéo jusqu'à 19secondes */
@@ -265,8 +285,58 @@ const Credits = function(allIMG){
             CreditRight.appendChild(imgDiv);
         }
     }
-    CreditLeft.style.animation = "translate 243s ease forwards";
-    CreditRight.style.animation = "translate 243s ease forwards";
+    CreditLeft.style.animation = "translate 157s linear forwards";
+    CreditRight.style.animation = "translate 157s linear forwards";
     document.body.appendChild(CreditLeft);
     document.body.appendChild(CreditRight);
 };
+
+const CreditsIMG = function(){
+    const ClasseDiv = document.getElementById("AllClasse");
+    const AllName = ClasseDiv.querySelectorAll('h4');
+    let top = 0
+    for (let i = 0; i < AllName.length; i++){
+        top++
+        random_number = Math.floor(Math.random() * 20)
+        const Maindiv = document.createElement("div");
+        Maindiv.classList.add("NameDiv")
+        const h3 = document.createElement("h3")
+        h3.classList.add("NameCredit")
+        h3.innerHTML = AllName[i].innerHTML
+        const div = document.createElement("div");
+        div.classList.add("ppDiv");
+        const img = document.createElement("img");
+        img.style.animation =`rotate ${5 + random_number}s linear infinite`
+        img.classList.add("imgPP");
+        if (i % 2 == 0){
+            if (i % 4 == 0){
+                div.style.left = `${25 + random_number}%`
+            }
+            else{
+                div.style.left = `${5 + random_number}%`
+            }
+        }
+        else{
+            if ((i-1)% 4 == 0){
+                div.style.right = `${25 + random_number}%`
+            }
+            else{
+                div.style.right = `${5 + random_number}%`
+            }
+        };
+        let direction = [`rotate(-${random_number-10}deg)`, `rotate(${random_number-10}deg)`];
+        div.style.transform = `${direction[Math.floor(Math.random() * 2)]}`;
+        const p = document.createElement("p");
+        Name = AllName[i].innerHTML;
+        var NameOnly = Name.split(' ')[0];
+        img.src = `./ClassroomPP/${NameOnly}.webp`;
+        p.innerHTML = NameOnly;
+        p.style.animation =`rotate ${5 + random_number}s linear infinite`
+        div.appendChild(img);
+        div.appendChild(p);
+        Maindiv.appendChild(h3)
+        Maindiv.appendChild(div)
+        AllName[i].insertAdjacentElement('afterend', Maindiv);
+    }
+}
+
